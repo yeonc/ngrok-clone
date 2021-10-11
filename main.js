5a6bf393a10f200ca5318b2e176370e99cd14cf2
yeonc <chsynee@gmail.com>
2021-10-11 17:00:18 +0900

    Use Typed.js to make text typed

---
+++ main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Typed.js
+const options = {
+  strings: [
+    'testing your chatbot. ^1500',
+    'SSH access to your Raspberry Pi. ^1500',
+    'building webhook integrations. ^1500',
+    'exposing your local web server. ^1500',
+    'testing on mobile devices. ^1500',
+    'sending previews to clients. ^1500',
+  ],
+  typeSpeed: 70,
+  backSpeed: 20,
+  loop: true,
+  showCursor: false,
+};
+const typed = new Typed('.typed', options);
